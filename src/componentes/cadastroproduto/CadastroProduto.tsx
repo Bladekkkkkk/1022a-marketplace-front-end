6a4c6c49c43c9372ff41fa61a1e083945276045e
Bladekkkkkk <116742943+Bladekkkkkk@users.noreply.github.com>
2025-01-28 15:15:02 -0400

    Update do código para ficar igual ao antigo trabalho
    Basicamente estou alterando os campos para que fiquem igual ao meu trabalho anterior.

--- src/componentes/cadastroproduto/CadastroProduto.tsx
+++ src/componentes/cadastroproduto/CadastroProduto.tsx
@@ -1,26 +1,32 @@
 import {  ChangeEvent, FormEvent, useState } from "react"
 import { useNavigate } from 'react-router-dom';
+
+
 function CadastroProduto(){
     const navigate = useNavigate()
-    const [id,setId] = useState("")
-    const [nome,setNome] = useState("")
-    const [descricao,setDescricao] = useState("")
-    const [preco,setPreco] = useState("")
-    const [imagem,setImagem] = useState("")
-    async function handleForm(event:FormEvent){
+    const [nome, setNome] = useState("")
+    const [marca, setMarca] = useState("")
+    const [tamanhotela, setTamanhotela] = useState("")
+    const [resolucaotela, setResolucaotela] = useState("")
+    const [proporcaotela, setProporcaotela] = useState("")
+    const [frequenciatela, setFrequenciatela] = useState("")
+    const [imagem, setImagem] = useState("")
+    async function handleForm(event: FormEvent){
         event.preventDefault()
-        try{
-            const resposta = await fetch("http://localhost:8000/produtos",{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
+        try {
+            const resposta = await fetch("http://localhost:8000/produtos", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
                 },
-                body:JSON.stringify({
-                    id:id,
-                    nome:nome,
-                    descricao:descricao,
-                    preco:preco,
-                    imagem:imagem
+                body: JSON.stringify({
+                    nome: nome,
+                    marca: marca,
+                    tamanhotela: tamanhotela,
+                    resolucaotela: resolucaotela,
+                    proporcaotela: proporcaotela,
+                    frequenciatela: frequenciatela,
+                    imagem: imagem
                 })
             })
             if(resposta.status!=500){
@@ -37,42 +43,107 @@ function CadastroProduto(){
         }
         
     }
-    function handleId(event:ChangeEvent<HTMLInputElement>){
-        setId(event.target.value)
-    }
-    function handleNome(event:ChangeEvent<HTMLInputElement>){
+    function handleNome(event: ChangeEvent<HTMLInputElement>) {
         setNome(event.target.value)
     }
-    function handleDescricao(event:ChangeEvent<HTMLInputElement>){
-        setDescricao(event.target.value)
+    function handleMarca(event: ChangeEvent<HTMLInputElement>) {
+        setMarca(event.target.value)
     }
-    function handlePreco(event:ChangeEvent<HTMLInputElement>){
-        setPreco(event.target.value)
+    function handleTamanhotela(event: ChangeEvent<HTMLInputElement>) {
+        setTamanhotela(event.target.value)
     }
-    function handleImagem(event:ChangeEvent<HTMLInputElement>){
+    function handleResolucaotela(event: ChangeEvent<HTMLInputElement>) {
+        setResolucaotela(event.target.value)
+    }
+    function handleProporcaotela(event: ChangeEvent<HTMLInputElement>) {
+        setProporcaotela(event.target.value)
+    }
+    function handleFrequenciatela(event: ChangeEvent<HTMLInputElement>) {
+        setFrequenciatela(event.target.value)
+    }
+    function handleImagem(event: ChangeEvent<HTMLInputElement>) {
         setImagem(event.target.value)
     }
     return(
         <>
-            <h1>Meu Componente de Cadastro de Produtos</h1>
-            <form onSubmit={handleForm}>
-                <div>
-                    <input placeholder="Id" type="text" name="id" id="id" onChange={handleId} />
-                </div>
-                <div>
-                    <input placeholder="Nome" type="text" name="nome" id="nome" onChange={handleNome} />
-                </div>
-                <div>
-                    <input placeholder="Descrição" type="text" name="descricao" id="descricao" onChange={handleDescricao} />
-                </div>
-                <div>
-                    <input placeholder="Preço" type="text" name="preco" id="preco" onChange={handlePreco} />
-                </div>
-                <div>
-                    <input placeholder="URL Imagem" type="text" name="imagem" id="imagem" onChange={handleImagem} />
-                </div>
-                <input type="submit" value="Cadastrar" />
-            </form>
+            <header className="site-header">
+       <div className="logo">
+        <span className="icon">🖥️</span>
+        <span className="logo-text">KM MONITORES</span>
+    </div>
+
+    <div className="search-bar">
+        <input type="text" placeholder="Busque aqui" />
+        <button className="search-button">➤</button>
+    </div>
+
+    <div className="header-actions">
+        <button
+            className="produto-button"
+            onClick={() => navigate("/produtos")}>
+            Produtos
+        </button>
+        <button
+            className="usuario-button"
+            onClick={() => navigate("/usuarios")}>
+            Usuários
+        </button>
+        <button
+            className="cadastroproduto-button"
+            onClick={() => navigate("/cadastro-produto")}>
+            Cadastrar Produtos
+        </button>
+        <button
+            className="login-button"
+            onClick={() => navigate("/cadastro-usuario")}>
+            Cadastrar-se
+        </button>
+
+        <div className="icons">
+            <a href="#" className="icon">🛒</a>
+            <a href="#" className="icon">❤️</a>
+        </div>
+    </div></header>
+
+
+
+      
+      <div className="container">
+      <h1>Cadastro de Produtos</h1>
+      <form onSubmit={handleForm}>
+    <div className="input-group">
+        <label htmlFor="nome">Nome</label>
+        <input placeholder="Nome" type="text" name="nome" id="nome" onChange={handleNome} />
+    </div>
+    <div className="input-group">
+        <label htmlFor="marca">Marca</label>
+        <input placeholder="Marca" type="text" name="marca" id="marca" onChange={handleMarca} />
+    </div>
+    <div className="input-group">
+        <label htmlFor="tamanhotela">Tamanho</label>
+        <input placeholder="Tamanho" type="text" name="tamanhotela" id="tamanhotela" onChange={handleTamanhotela} />
+    </div>
+    <div className="input-group">
+        <label htmlFor="resolucaotela">Resolução</label>
+        <input placeholder="Resolução" type="text" name="resolucaotela" id="resolucaotela" onChange={handleResolucaotela} />
+    </div>
+    <div className="input-group">
+        <label htmlFor="proporcaotela">Proporção</label>
+        <input placeholder="Proporção" type="text" name="proporcaotela" id="proporcaotela" onChange={handleProporcaotela} />
+    </div>
+    <div className="input-group">
+        <label htmlFor="frequenciatela">Frequência</label>
+        <input placeholder="Frequência" type="text" name="frequenciatela" id="frequenciatela" onChange={handleFrequenciatela} />
+    </div>
+    <div className="input-group">
+        <label htmlFor="imagem">URL Imagem</label>
+        <input placeholder="URL Imagem" type="text" name="imagem" id="imagem" onChange={handleImagem} />
+    </div>
+    <input type="submit" value="Cadastrar" className="submit-btn" />
+    </form>
+    
+    </div>
+
         </>
     )
 }
