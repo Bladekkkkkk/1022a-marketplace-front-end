54ac692613156871031493d56456044ba2bfa73f
Kauã Santos <116742943+Bladekkkkkk@users.noreply.github.com>
2025-02-04 10:38:16 -0400

    arrumando escrita e seperando os usuários dos produtos

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,8 @@ type ProdutoType = {
   imagem: string
 }
 
-// Tipo para usuários
-type UsuarioType = {
-  id: number,
-  name: string,
-  email: string,
-  created_at: string,
-  updated_at: string
-}
-
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
-  const [usuarios, setUsuarios] = useState<UsuarioType[]>([])
 
   // useEffect para carregar produtos e usuários
   useEffect(() => {
@@ -33,11 +23,6 @@ function App() {
     fetch("https://one022a-marketplace-18yz.onrender.com/produtos")
       .then(resposta => resposta.json())
       .then(dados => setProdutos(dados))
-
-    // Buscar os usuários
-    fetch("https://one022a-marketplace-18yz.onrender.com/usuarios")
-      .then(resposta => resposta.json())
-      .then(dados => setUsuarios(dados))
   }, [])
 
   // Função para excluir um produto
@@ -76,23 +61,6 @@ function App() {
                 <button className="botao-comprar">Comprar</button>
                 <button className="botao-excluir" onClick={() => handleExcluirProduto(produto.id)}>Excluir</button>
                 <Link className="botao-alterar" to={`/alterar-produto/${produto.id}`}>Alterar</Link>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-
-      {/* Listagem de Usuários */}
-      <div className="usuarios-container">
-        <h1 className='titulo-usuario'>Usuários</h1>
-        <div className="usuarios-list">
-          {
-            usuarios.map(usuario => (
-              <div key={usuario.id} className="usuario-item">
-                <h1 className="usuario-nome">{usuario.name}</h1>
-                <p>Email: {usuario.email}</p>
-                <p>Criado em: {new Date(usuario.created_at).toLocaleDateString()}</p>
-                <p>Atualizado em: {new Date(usuario.updated_at).toLocaleDateString()}</p>
               </div>
             ))
           }
